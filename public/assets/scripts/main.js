9cac6aff01c1e54da1ff2e8ce6f31bb218750bf7
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2017-08-16 14:51:16 -0500

    feat: add basic markup of funcionalities

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,3 +1,46 @@
 //
 // Main JavaScript file
 // --------------------------------------------------------
+var PLTRFM = {
+  init: function () {
+    (window.innerWidth < 768) ? PLTRFM.Sliders.init() : PLTRFM.Clicker.init();
+  },
+
+  Clicker: {
+    init: function () {
+      this.methods.init();
+    },
+    state: {
+      index: 0,
+    },
+    methods: {
+      init: function () {
+        PLTRFM.Phones.clickEl.querySelector('img').className +="active";
+      }
+    }
+  },
+
+  Phones: {
+    swipeEl: document.querySelector("#slider"),
+    clickEl: document.querySelector('.bigPhonePic')
+  },
+
+  Sliders: {
+    init: function () {
+       Swipe(PLTRFM.Phones.swipeEl, PLTRFM.Sliders.phones.options);
+    },
+    options: {
+      startSlide: 0,
+      speed: 400,
+      draggable: false,
+      continuous: false,
+      disableScroll: false,
+      stopPropagation: true,
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+
+    }
+  }
+}
+
+PLTRFM.init()
