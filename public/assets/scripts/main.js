dd481c8f0ed606b32d3259f17ef011dbbabcfa22
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2017-08-21 12:00:55 -0500

    feat: scroll nappels/pltfm-www#19

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -97,6 +97,22 @@ var PLTFRM = {
         }
       }
     }
+  },
+  Scroller: {
+    toBottom: function () {
+      PLTFRM.Scroller.scrollTo(document.body, document.body.offsetHeight - window.innerHeight, 400);
+    },
+    scrollTo :function (element, to, duration) {
+      if (duration <= 0) return;
+      var difference = to - element.scrollTop;
+      var perTick = difference / duration * 10;
+
+      setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        PLTFRM.Scroller.scrollTo(element, to, duration - 10);
+      }, 10);
+    }
   }
 
 }
